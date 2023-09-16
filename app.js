f51a2f35bffeca143f35bdf71d7cdcd3972687f9
Miguel Ardila <miguel_sxe@hotmail.com>
2023-09-15 21:24:40 -0500

    función initUI

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
-const {initUI} = require('./modules/punto-de-entrada');
+// Importa la función initUI desde el archivo punto-de-entrada.js en la carpeta modules.
+const { initUI } = require('./modules/punto-de-entrada');
 
+// Ejecuta la función initUI que se importó anteriormente.
 initUI();
